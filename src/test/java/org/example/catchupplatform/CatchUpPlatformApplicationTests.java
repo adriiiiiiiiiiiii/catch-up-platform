2a1b21eafba3223eafe683b94bee21c49dc0a2f0
Adriana Diestra <adriana10tra@gmail.com>
2024-10-10 22:47:45 -0500

    initial comit

---
+++ src/test/java/org/example/catchupplatform/CatchUpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.catchupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchUpPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
